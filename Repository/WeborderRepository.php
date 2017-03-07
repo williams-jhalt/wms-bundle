5b2779539c1e678081f28d740b3af3d937595dd0
John Halterman <johnh@williams-trading.com>
2017-03-07 15:48:11 -0500

    reporting fix

--- Repository/WeborderRepository.php
+++ Repository/WeborderRepository.php
@@ -44,7 +44,7 @@ class WeborderRepository {
      */
     public function findByOrderDate(DateTime $startDate, DateTime $endDate) {
 
-        $limit = 100;
+        $limit = 500;
         $offset = 0;
 
         do {
